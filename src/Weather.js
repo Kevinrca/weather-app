82f5dd34e10bda0676730b77b3d766cf4d74458a
Kévin Rocha <neonix75@gmail.com>
2020-08-13 14:50:42 +0200

    Display the elements of the app

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./css/weather.css"
+import weatherIcone from "./images/weather_icones/10d.png"
 
 function Weather() {
     return (
         <div className="weatherCard">
-
+            <img className="weatherIcone" src={weatherIcone} alt="weatherIcone"></img>
+            <p className="temperature">21°<span className="tempUnit"> c</span></p>
+            <p className="description">Temps clair</p>
+            <p className="city">Paris, <span className="country">France</span></p>
         </div>
     )
 }
