e81603451b5545b16f96a531ccfc0667b64addb0
Kévin Rocha <neonix75@gmail.com>
2020-08-13 13:26:23 +0200

    cleanning the workspace

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      
     </div>
   );
 }
