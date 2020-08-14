037bb174a5a088ee211e7bddd49b5f08b95a7f66
Kévin Rocha <neonix75@gmail.com>
2020-08-14 23:07:32 +0200

    Correctly setting up the api call

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Weather from './Weather.jsx';
 import './css/App.css';
 
 import unknown from './images/weather_icones/unknown.png';
 
+const api = {
+  key: "646d82915c35c27e56f30d7478d9c087",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 // http://ip-api.com/json
 // http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=646d82915c35c27e56f30d7478d9c087
 
 
 function App() {
-
-  let city = "Paris";
-
+  // const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
+  
+  async function getWeather(city) {
+    await fetch(`${api.base}weather?q=${city}&lang=fr&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        console.log(result);
+      })
+ }
 
-  const search = () => {
-      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=646d82915c35c27e56f30d7478d9c087`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          console.log(result);
-        });
-  }
-
-  useEffect(() => {
-    search();
-  }, [])
-
-
-  window.addEventListener("keyup", e => {
-    e.preventDefault();
-    city = "Marseille";
-    search();
+  window.addEventListener("load", () => {
+    fetch(`http://ip-api.com/json`)
+      .then(result => result.json())
+      .then(json => {
+        getWeather(json.city);
+      });
   })
-
 
 
   return (
