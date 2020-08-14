efe64cc58f694208bf6a8638908fad2cd169a357
Kévin Rocha <neonix75@gmail.com>
2020-08-14 11:47:45 +0200

    Passing the weather api informations between the app.js and the weather.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Weather from './Weather.jsx';
 import './css/App.css';
 
 function App() {
+
+  const [weatherTemp, getTemp] = useState("-");
+  const [weatherDescription, getDescription] = useState("-");
+  const [weatherCity, getCity] = useState("-");
+  const [weatherCountry, getCountry] = useState("")
+    
+    
+  async function getWeather() {
+    const city = await fetch(`http://ip-api.com/json`)
+                  .then(result => result.json())
+                  .then(json => json.city);
+            
+    const weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=646d82915c35c27e56f30d7478d9c087`)
+                  .then(result => result.json())
+                  .then(data => data);
+        
+    getTemp(prev => prev = Math.floor(weather.main.temp - 273));
+    getDescription(prev => prev = weather.weather[0].description);
+    getCity(prev => prev = weather.name);
+    getCountry(prev => prev = weather.sys.country);
+  }
+  getWeather();
+  
+  
   return (
     <div className="app">
       <h1 className="websiteTitle">Weather app</h1>
-      <Weather />
+      <Weather 
+        weatherTemp={weatherTemp}
+        weatherDescription={weatherDescription}
+        weatherCity={weatherCity}
+        weatherCountry={weatherCountry} />
     </div>
   );
 }
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,43 +1,17 @@
-import React, { useState, Suspense } from 'react'
+import React from 'react'
 import "./css/weather.css"
 import weatherIcone from "./images/weather_icones/10d.png"
 
 
 
-function Weather() {
-
-    const [weatherTemp, getTemp] = useState("-");
-    const [weatherDescription, getDescription] = useState("-");
-    const [weatherCity, getCity] = useState("-");
-    const [weatherCountry, getCountry] = useState("")
-    
-    async function getWeather(prev) {
-        const city = await fetch(`http://ip-api.com/json`)
-                        .then(result => result.json())
-                        .then(json => json.city);
-            
-        const weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=646d82915c35c27e56f30d7478d9c087`)
-                        .then(result => result.json())
-                        .then(data => data);
-        
-        getTemp(prev => prev = Math.floor(weather.main.temp - 273));
-        getDescription(prev => prev = weather.weather[0].description);
-        getCity(prev => prev = weather.name);
-        getCountry(prev => prev = weather.sys.country);
-    }
-    getWeather();
-
-
-
-
-
+function Weather({weatherTemp, weatherDescription, weatherCity, weatherCountry}) {
 
     return (
         <div className="weatherCard">
             <img className="weatherIcone" src={weatherIcone} alt="weatherIcone"></img>
             <p className="temperature">{weatherTemp}°<span className="tempUnit"> c</span></p>
             <p className="description">{weatherDescription}</p>
-    <p className="city">{weatherCity + ", "} <span className="country">{weatherCountry}</span></p>
+            <p className="city">{weatherCity + ", "} <span className="country">{weatherCountry}</span></p>
         </div>
     )
 }
